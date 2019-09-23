9e194a2f170a09dc853562a6e56dc2022a8a73c7
devgustavofarias@gmail.com <devgustavofarias@gmail.com>
2019-09-22 23:14:20 -0300

    Interface folder removed

--- interface/src/index.js
+++
@@ -1,8 +0,0 @@
-import './reset.css';
-import './style.css';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './routes';
-
-ReactDOM.render(<Routes />, document.getElementById('root'));
